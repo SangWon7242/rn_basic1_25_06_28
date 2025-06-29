c81a98a1379cc380fd326329412eba3d11811300
SangWon7242 <gotkdqja@gmail.com>
2025-06-29 12:36:39 +0900

    날씨 app, header 구현

--- app/index.tsx
+++ app/index.tsx
@@ -1,17 +1,20 @@
-import { StyleSheet, View } from "react-native";
-
-// View : 전체를 감싸는 컨테이너
-// Text : 텍스트를 표시하는 컴포넌트
-// - text를 감쌀 때는 Text 컴포넌트를 사용
-// style : 스타일을 적용하는 props
+import { StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
   return (
     <>
       <View style={[styles.container]}>
-        <View style={{ flex: 1, backgroundColor: "red" }} />
-        <View style={{ flex: 1, backgroundColor: "blue" }} />
-        <View style={{ flex: 1, backgroundColor: "green" }} />
+        <View style={styles.header}>
+          <View style={styles.headerTop}>
+            <Text style={styles.cityName}>Ansan</Text>
+          </View>
+          <View style={styles.headerBottom}>
+            <Text style={styles.regDate}>일요일, 2025-06-29</Text>
+            <Text style={styles.weather}>맑음</Text>
+          </View>
+        </View>
+        <View style={styles.mainWrap}></View>
+        <View style={styles.footer}></View>
       </View>
     </>
   );
@@ -20,5 +23,54 @@ export default function Index() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: "#fee142",
+  },
+  header: {
+    flex: 1,
+    backgroundColor: "#fee142",
+    borderWidth: 3,
+    borderColor: "red",
+  },
+  headerTop: {
+    flex: 1,
+  },
+  cityName: {
+    fontSize: 40,
+    fontWeight: "bold",
+    textAlign: "center",
+    padding: 10,
+  },
+  headerBottom: {
+    flex: 4,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  regDate: {
+    fontSize: 15,
+    fontWeight: "bold",
+    color: "#fee142",
+    backgroundColor: "#000",
+    borderRadius: 50,
+    paddingBlockStart: 5,
+    paddingBlockEnd: 10,
+    paddingInlineStart: 10,
+    paddingInlineEnd: 10,
+  },
+  weather: {
+    fontSize: 30,
+    fontWeight: "bold",
+    marginBlockStart: 10,
+  },
+  mainWrap: {
+    flex: 2,
+    backgroundColor: "#fee142",
+    borderWidth: 3,
+    borderColor: "blue",
+  },
+  footer: {
+    flex: 1,
+    backgroundColor: "#fee142",
+    borderWidth: 3,
+    borderColor: "green",
   },
 });
